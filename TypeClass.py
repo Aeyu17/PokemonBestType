2a5df163c174dc176bbc384063496c74ce0221e5
Aeyu17 <jami3507@gmail.com>
2022-11-30 14:33:22 -0600

    more

--- TypeClass.py
+++ TypeClass.py
@@ -1,11 +1,11 @@
 import pkmntypes
+import csv
 
 class Type(object):
     def __init__(self, primary, secondary=pkmntypes.null):
         """
         primary and secondary are dictionaries
         """
-
         try:
             self.typeCombo = (
                 pkmntypes.typeKeys[pkmntypes.typeList.index(primary)],
@@ -35,7 +35,10 @@ class Type(object):
                 self.resistList.append(pkmntype)
             else:
                 self.neutralList.append(pkmntype)
-        
+
+        self.offensiveScore = 0
+        self.defensiveScore = 0
+
     def __str__(self):
         if self.typeCombo[1] != "null":
             return f"""Types: {self.typeCombo[0]}, {self.typeCombo[1]}
@@ -57,26 +60,16 @@ Neutral: {self.neutralList}
     def getMatchups(self):
         return self.matchups
 
-    def isWeakTo(self, pkmntype):
-        return pkmntype in self.weakList
-
-    def isResistantTo(self, pkmntype):
-        return pkmntype in self.resistList
-
-    def isImmuneTo(self, pkmntype):
-        return pkmntype in self.immuneList
-
-    def isNeutralTo(self, pkmntype):
-        return pkmntype in self.neutralList
-
     def getMultiplier(self, pkmntype):
         return self.matchups[pkmntype]
 
-    def getPrimaryType(self):
-        return self.typeCombo[0]
+    def getType(self):
+        return (self.typeCombo[0], self.typeCombo[1])
 
-    def getSecondaryType(self):
-        return self.typeCombo[1]
+    def getScore(self):
+        return (self.offensiveScore, self.defensiveScore)
+
+    
     
 def challenge(pkmn1, pkmn2):
     print("POKEMON 1")
@@ -85,26 +78,63 @@ def challenge(pkmn1, pkmn2):
     print("POKEMON 2")
     print(pkmn2)
 
-    print("POKEMON 1 ATTACKING POKEMON 2")
-    
+    attackTypes = pkmn1.getType()
+    for atktype in attackTypes:
+        if atktype == 'null':
+            break
+        multiplier = pkmn2.getMultiplier(atktype)
+        print(multiplier)
+        if multiplier == 4.:
+            pkmn1.offensiveScore += 2
+            pkmn2.defensiveScore -= 2
+        elif multiplier == 2.:
+            pkmn1.offensiveScore += 1
+            pkmn2.defensiveScore -= 1
+        elif multiplier == 0.5:
+            pkmn1.offensiveScore -= 1
+            pkmn2.defensiveScore += 1
+        elif multiplier == 0.25:
+            pkmn1.offensiveScore -= 2
+            pkmn2.defensiveScore += 2
+        elif multiplier == 0.:
+            pkmn1.offensiveScore -= 3
+            pkmn2.defensiveScore += 3
 
 
+def runSim():
+    allPkmn = []
+    # should be in the form {Type(Grass, Dark): (2, 4)} where the value is (OffensiveScore, DefensiveScore)
+    # for i in range(len(pkmntypes.typeList)):
+    #     for j in range(i, len(pkmntypes.typeList)):
+    #         ptype = pkmntypes.typeList[i]
+    #         stype = pkmntypes.typeList[j]
+    for i, ptype in enumerate(pkmntypes.typeList):
+        for stype in pkmntypes.typeList[i:]:
+            if ptype == stype:
+                pkmn = Type(ptype)
+            else:
+                pkmn = Type(ptype, stype)
+            print(pkmn)
+            allPkmn.append(pkmn)       
+
+    for attacker in allPkmn:
+        for defender in allPkmn:
+            challenge(attacker, defender)
+            # print(attacker)
+            # print(attacker.getScore())
+            # print(defender)
+            # print(defender.getScore())
+
+    with open('types.csv', 'w') as f:
+        writer = csv.writer(f)
+        for pkmn in allPkmn:
+            print(pkmn)
+            print(pkmn.getScore())
+            writer.writerow([pkmn.getType(), pkmn.getScore()])
+        
 
 
 
 if __name__ == "__main__":
-    pkmn1 = Type(pkmntypes.grass, pkmntypes.dark)
-    print(pkmn1)
-    print(pkmn1.getMatchups())
-    print(pkmn1.isWeakTo('fire'))
-    print(pkmn1.isWeakTo('grass'))
-    print(pkmn1.isResistantTo('grass'))
-    print(pkmn1.isResistantTo('electric'))
-    print(pkmn1.isImmuneTo('grass'))
-    print(pkmn1.isImmuneTo('psychic'))
-    print(pkmn1.isNeutralTo('grass'))
-    print(pkmn1.isNeutralTo('rock'))
-    print(pkmn1.getMultiplier('normal'))
-    print(pkmn1.getMultiplier('bug'))
-
+    runSim()
 
